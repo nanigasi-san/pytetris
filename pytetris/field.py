ca217d51738a16156e709e2d8dcc265ffd9508e2
nanigasi_san <nanigasi.py@gmail.com>
2020-04-10 08:40:14 +0900

    Tetris field (#5)
    * del circleci
    
    * Fieldクラスの実装
    
    * add test
    
    * lint
    
    * Update pythonpackage.yml

---
+++ pytetris/field.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class Field:
+    def __init__(self):
+        self.field = np.zeros((20, 10))
+
+    def update(self):
+        self.field = np.array(([line if sum(line) < 10 else [0] * 10 for line in self.field]))

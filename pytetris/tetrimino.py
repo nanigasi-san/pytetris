809e9f4489225c57b51e9fd1b90913627b37bd09
nanigasi_san <nanigasi.py@gmail.com>
2020-04-10 08:27:50 +0900

    空行、空列が無いように変更

--- pytetris/tetrimino.py
+++ pytetris/tetrimino.py
@@ -3,9 +3,15 @@ class Tetrimino:
     def __init__(self, cell):
         if (cell < 0).all() or (cell > 1).all():
             raise ValueError
-        elif cell.shape != (4, 4):
-            raise ValueError
-        self.cell = cell
+        else:
+            line, column = cell.shape
+            for i in range(line):
+                if sum(cell[i]) == 0:
+                    raise ValueError
+            for i in range(column):
+                if np.sum([cell[:][i]]) == 0:
+                    raise ValueError
+            self.cell = cell
 
     def rot90(self):
         self.cell = np.rot90(self.cell)
--- tests/test_tetrimino.py
+++ tests/test_tetrimino.py
@@ -10,15 +10,15 @@ def test_tetrimino_init_1():
 
 
 def test_tetrimino_init_2():
-    cell = np.zeros((4, 4))
+    cell = np.array([[0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1], [1, 0, 1, 0]])
     tetrimino = Tetrimino(cell)
     assert (tetrimino.cell == cell).all()
 
 
 def test_tetrimino_init_3():
-    cell = np.array([[0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1], [1, 0, 1, 0]])
-    tetrimino = Tetrimino(cell)
-    assert (tetrimino.cell == cell).all()
+    cell = np.zeros((4, 4))
+    with pytest.raises(ValueError):
+        Tetrimino(cell)
 
 
 def test_tetrimino_init_4():
@@ -35,7 +35,7 @@ def test_tetrimino_init_5():
 
 @pytest.fixture
 def rot_base_cell():
-    return np.array([[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [1, 1, 1, 1]])
+    return np.array([[0, 1, 0, 1], [0, 1, 0, 1], [0, 1, 0, 1], [0, 1, 0, 1]])
 
 
 def test_tetrimino_rot_1(rot_base_cell):

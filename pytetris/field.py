fd1bac3e1583159e87d94bdd12546919e6200a0a
nanigasi_san <nanigasi.py@gmail.com>
2020-04-10 09:06:04 +0900

    tetrimino加算と更新の実装、テスト

--- pytetris/field.py
+++ pytetris/field.py
@@ -7,3 +7,11 @@ class Field:
 
     def update(self):
         self.field = np.array(([line if sum(line) < 10 else [0] * 10 for line in self.field]))
+
+    def add(self, start, tetrimino):
+        l, c = tetrimino.cell.shape
+        x, y = start
+        for i in range(l):
+            for j in range(c):
+                self.field[x+i][y+j] = 1
+                print(x+i, y+j)
--- tests/test_field.py
+++ tests/test_field.py
@@ -1,6 +1,7 @@
 from pytetris.field import Field
+from pytetris.tetrimino import Tetrimino
 import numpy as np
-
+import pytest
 
 def test_field_init_1():
     vaild_field = np.zeros((20, 10))
@@ -26,3 +27,28 @@ def test_field_del_line_2():
     assert (field.field == vaild_field).all()
 
 
+@pytest.fixture
+def O():
+    cell = np.ones((2, 2))
+    return Tetrimino(cell)
+
+
+def test_add_tetrimino_1(O):
+    valid_field = np.array([[0.0 for j in range(10)] for i in range(20)])
+    valid_field[-1][0] = 1.0
+    valid_field[-1][1] = 1.0
+    valid_field[-2][0] = 1.0
+    valid_field[-2][1] = 1.0
+    field = Field()
+    field.add((18, 0), O)
+    assert (field.field == valid_field).all()
+
+
+def test_add_update_tetrimino_1(O):
+    valid_field = np.zeros((20, 10))
+    field = Field()
+    for i in range(5):
+        field.add((18, 2*i), O)
+    field.update()
+    print(type(field.field), type(valid_field))
+    assert (field.field == valid_field).all()

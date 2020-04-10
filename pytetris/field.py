50a708487ad4fad88a92d46f9f49e6e5fc275d8e
nanigasi_san <nanigasi.py@gmail.com>
2020-04-10 10:18:46 +0900

    is_delpyableを実装

--- pytetris/field.py
+++ pytetris/field.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class Field:
     def __init__(self):
         self.field = np.zeros((20, 10))
@@ -9,9 +8,23 @@ class Field:
         self.field = np.array(([line if sum(line) < 10 else [0] * 10 for line in self.field]))
 
     def add(self, start, tetrimino):
+        if not self.is_deployable(start, tetrimino):
+            raise TetriminoExistError
         l, c = tetrimino.cell.shape
         x, y = start
         for i in range(l):
             for j in range(c):
-                self.field[x+i][y+j] = 1
-                print(x+i, y+j)
+                self.field[x+i][y+j] += tetrimino.cell[i][j]
+
+    def is_deployable(self, start, tetrimino):
+        l, c = tetrimino.cell.shape
+        x, y = start
+        for i in range(l):
+            for j in range(c):
+                if self.field[x+i][y+j] + tetrimino.cell[i][j] > 1:
+                    return False
+        return True
+
+
+class TetriminoExistError(Exception):
+    pass
